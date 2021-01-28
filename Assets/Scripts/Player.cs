c78ab53dc6d39aa7a84598b2285b8d7cb2de27fc
Alam Rodriguez <alamgrodriguez@gmail.com>
2021-01-28 11:54:48 -0600

    Movimiento jugador V1
    -[Feature] Script "Player.cs". El jugador se puede sumergir presionando Click Izquierdo, y subirá automáticamente cuando no se esté presionando.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float fuerzaDeFlote; //Fuerza con la que el Player es empujado a la superficie constantemente
+
+    public Rigidbody2D rb; //RigidBody del OBJETO PADRE (IMPORTANTE)
+    private float fuerzaVertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuerzaVertical = fuerzaDeFlote; //Por defecto va a flotar
+        //this.transform.rotation = Quaternion.Euler(0f, 0f, 30f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //El click derecho te hace ir hacia abajo, si lo sueltas empiezas a flotar
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            //this.transform.rotation = Quaternion.Euler(0f, 0f, -30f);
+            fuerzaVertical = -fuerzaDeFlote;
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            //this.transform.rotation = Quaternion.Euler(0f, 0f, 30f);
+            fuerzaVertical = fuerzaDeFlote;
+        }
+
+        rb.velocity = Vector2.up * fuerzaVertical;
+    }
+
+    
+    
+}
