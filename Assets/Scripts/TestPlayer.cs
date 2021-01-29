821e408c70967f9bb7810ae5b46379faf444d751
Alam Rodriguez <alamgrodriguez@gmail.com>
2021-01-29 10:44:21 -0600

    Movimiento y prefab de jugador
    -[Improve] Modificado el movimiento, ahora es libre, manejado con WASD.
    -[Improve] Nuevo prefab de PLAYER_1

--- Assets/Scripts/TestPlayer.cs
+++ Assets/Scripts/TestPlayer.cs
@@ -10,17 +10,22 @@ public class TestPlayer : MonoBehaviour
 
     void Update()
     {
+        //Este movimiento es con gravedad invertida estilo Flappy Bird
         #region MOVIMIENTO VERSIÓN 2
-        if (Input.GetKeyDown(KeyCode.Mouse0) )
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             rb.velocity = Vector2.down * fuerzaDeSumergir;
         }
-        #endregion
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.zero;
+            transform.position = new Vector2(transform.position.x, 0f);
+        }
         if (rb.velocity.y > 0.1f)
         {
             rb.velocity = new Vector2(0f, rb.velocity.y + fuerzaSubidaExtra);
         }
+        #endregion
 
     }
 
