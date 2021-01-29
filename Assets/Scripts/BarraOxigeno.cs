e52fede70dc597f0f7f35d3a7a191789d1bc86bd
Seegikan <62976082+Seegikan@users.noreply.github.com>
2021-01-29 10:45:11 -0600

    Barra de oxigeno, nueva escena de alezander

---
+++ Assets/Scripts/BarraOxigeno.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BarraOxigeno : MonoBehaviour
+{
+    private float oxigenoMax;
+    public float oxigenoActual;
+
+    Image barraOxigeno;
+
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        barraOxigeno = GetComponent<Image>();
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oxigenoActual = player.oxigeno;
+        barraOxigeno.fillAmount = oxigenoActual / oxigenoMax;
+    }
+}
