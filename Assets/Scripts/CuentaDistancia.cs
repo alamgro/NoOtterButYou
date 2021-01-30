52c7066b55690dde3ed36ff41be6cdf67e9c9239
Seegikan <62976082+Seegikan@users.noreply.github.com>
2021-01-29 21:39:03 -0600

    Cuenta de distancia

---
+++ Assets/Scripts/CuentaDistancia.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CuentaDistancia : MonoBehaviour
+{
+    public float oxigenoMax; //Tamano total de barra 
+
+    float tiempo;
+    Image barraOxigeno;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiempo = 0;
+        barraOxigeno = GetComponent<Image>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo += Time.deltaTime;
+        print("El tiempo" + tiempo);
+        barraOxigeno.fillAmount = tiempo / oxigenoMax;
+
+    }
+    
+
+   
+    
+}
