8d33bacb8737319c2554772913dd18fe62b81816
Seegikan <62976082+Seegikan@users.noreply.github.com>
2021-01-30 21:22:07 -0600

    Escenas
    Todas pulidas
    Creditos con vinculos
    Mapping con imagen
    Mapping additive sin fondo
    Menu completo
    Pause additive sin fondo

---
+++ Assets/Scripts/HiperVinculo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiperVinculo : MonoBehaviour
+{
+	/*
+	 La funcion nos manda a un hiper vinculo a web,  para darnos a conocer en dieferentes medio
+	 */
+	public string links;
+
+	// Agregas
+	public void mandar()
+	{
+
+		Application.OpenURL(links);
+
+	}
+}
